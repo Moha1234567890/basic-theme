d433704d64a45e6e01c9a99cdb353e14479e155d
mohamed <mohame@yahoo.com>
2020-06-04 01:35:17 +0200

    add theme

---
+++ Project1-prsime/script.js
@@ -0,0 +1,19 @@
+function HoverButton()
+{
+	document.getElementById('fuck').style.backgroundColor = "#080";
+}
+function Hover()
+{
+	document.getElementById('fuck').style.backgroundColor = "#00BCBA";
+}
+
+function Show()
+{
+	document.getElementById("Click").style.visibility="visible";
+}
+
+function Hide()
+{
+	document.getElementById("Click").style.visibility="hidden"
+}
+
